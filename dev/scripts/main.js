54f2406e7426f093d4949b43103e1b2e54f50f2d
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-12-05 23:28:27 -0500

    adding home page

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
-	var yellowImg = document.getElementById('yellowLady');
-	setInterval(function() {
-		yellowLady.style.display = (yellowLady.style.display == 'none' ? '' : 'none');
-	}, 200);
+	// var yellowImg = document.getElementById('yellowLady');
+	// setInterval(function() {
+	// 	yellowLady.style.display = (yellowLady.style.display == 'none' ? '' : 'none');
+	// }, 200);
 
 });
 
 
-App.init = () => {
-	App.events();
-} //end of init();
-
-$(function() {
-	App.init();
+$('.allTheText').hover(function() {
+		$(this).find('.heroText1').hide();
+		$(this).find('.heroTextInner1').hide();
+		$(this).find('.heroText2').show();
+		$(this).find('.heroTextInner2').show();
+		$(this).find('.heroText3').show();
+		$(this).find('.heroTextInner3').show();
+		$(this).find('.heroText4').show();
+		$(this).find('.heroTextInner4').show();
+	}, function() {
+		$(this).find('.heroText1').show();
+		$(this).find('.heroTextInner1').show();
+		$(this).find('.heroText2').hide();
+		$(this).find('.heroTextInner2').hide();
+		$(this).find('.heroText3').hide();
+		$(this).find('.heroTextInner3').hide();
+		$(this).find('.heroText4').hide();
+		$(this).find('.heroTextInner4').hide();
 });
+
+$('.allTheText').hover(
+	function(){ $(this).addClass('hover') },
+	function(){ $(this).removeClass('hover') }
+)
+
+// App.init = () => {
+// 	App.events();
+// } //end of init();
+
+// $(function() {
+// 	App.init();
+// });
